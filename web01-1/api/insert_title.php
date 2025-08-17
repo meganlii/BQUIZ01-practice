78ba8ac16b147d072dc3b97aa29c8edf61bdf26e
meganlii <lovecezanne@gmail.com>
2025-08-17 18:46:19 +0800

    8/17更新-複習6/13檔案上傳功能

--- web01-1/api/insert_title.php
+++ web01-1/api/insert_title.php
@@ -13,15 +13,16 @@ echo "</pre>";
 
 // 步驟1 檢查 / !empty( ) 檢查是否有檔案上傳 
 // $_FILES['img'] = 上傳的檔案資訊
-// ['tmp_name'] = 檔案暫時存放的位置 / 當檔案上傳後，會先存在暫時位置
+// ['tmp_name'] = 檔案暫存區 / 當檔案上傳後會先存在暫存區  並由伺服器給予一組亂碼的檔名
 // !empty() = 檢查是否「不是空的」
 // 意思：如果用戶真的有選檔案上傳，就執行下面的程式
 if(!empty($_FILES['img']['tmp_name'])){
     
-    // 步驟2 移動 / move_uploaded_file( ) 移動檔案到正確位置
-    // 把檔案從暫時位置移到 ../images/ 資料夾
-    // $_FILES['img']['name'] = 檔案的原始名稱
-    move_uploaded_file($_FILES['img']['tmp_name'],"../images/".$_FILES['img']['name']);
+    // 步驟2 移動 / move_uploaded_file( ) 移動 上傳完成檔案 到 指定目錄下，並指定檔名
+    // 兩個參數 ( $from , $to )
+    // 把檔案從暫存區移到 ../images/ 資料夾
+    // $_FILES['img']['name'] = 檔案的原始名稱 可能帶副檔名
+    move_uploaded_file($_FILES['img']['tmp_name'], "../images/".$_FILES['img']['name']);
     
     // 步驟3 記錄 / 記錄檔案名稱
     // 把右方檔案名稱 存到 $_POST['img'] 變數中
--- web01-1/modal/title.php
+++ web01-1/modal/title.php
@@ -1,11 +1,13 @@
+<!-- 此功能搭配筆記 11401-FILE-mynote\upload\01-upload.php -->
 <!-- 步驟1 新增 資料夾madal跟title.php ./madal/title.php -->
 <!-- ./view2 <h3><form> 移到此頁 -->
 <h3 style='text-align:center'>新增標題區</h3>
 <hr>
 
-<!-- 步驟2 補足 <form> 參數  post資料送到./api/insert_title.php -->
+<!-- 步驟2 補足 <form> 參數  表單資料post送到api處理 ./api/insert_title.php -->
 <!-- 路徑不是../api  因為彈出視窗點開網址在 backend.php -->
 <!-- 使用ajax另外取得  傳到後端title.php  title.php再被引入到backend.php -->
+<!-- "multipart/form-data" 多媒體格式/表單資料 配合type="file"才會用編碼方式傳送file 一般表單不用加 -->
 <form action="./api/insert_title.php" method="post" enctype="multipart/form-data">
   <div>
     <label>標題區圖片</label>

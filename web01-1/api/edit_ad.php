721e03a468f99945729cac37a9d82b29c6a6c937
meganli <lovecezanne@gmail.com>
2025-09-02 13:55:12 +0800

    9/2更新

--- web01-1/api/edit_ad.php
+++ web01-1/api/edit_ad.php
@@ -14,46 +14,43 @@ include_once './db.php';
 dd($_POST);
 
 foreach ($_POST['id'] as $key => $id) {
-  
+
   // isset()跟in_array() 搭配使用 
-  if(isset($_POST['del']) && in_array($id,$_POST['del'])) {
-    
-  // 步驟1：將所有 $Title 改成 $Ad
-  $Ad->del($id);
+  if (isset($_POST['del']) && in_array($id, $_POST['del'])) {
 
-  }else{
+    // 步驟1：將所有 $Title 改成 $Ad
+    $Ad->del($id);
+  } else {
 
-  $row=$Ad->find($id);
-  
-  // 印出 從資料表拿出的$row
-  dd($row);
+    $row = $Ad->find($id);
 
-  // $row['text']資料庫的替代文字改為完成動態文字廣告  對應  $_POST表單送來的資料  對照截圖看
-  $row['text']=$_POST['text'][$key];
+    // 印出 從資料表拿出的$row
+    dd($row);
 
-  // 步驟2：修改$row['sh'] 可多選  之前sh不是陣列sh[]
-  // 修改三元運算式
-  
-  // 寫法1：$row['sh']=($_POST['sh']==$id)?1:0;
-  // 要加上in_array() 確認迴圈['id'] 是否在$_POST['sh']陣列裡面 ~ 還要搭派isset()是否存在 一起使用
+    // $row['text']資料庫的替代文字改為完成動態文字廣告  對應  $_POST表單送來的資料  對照截圖看
+    $row['text'] = $_POST['text'][$key];
 
-  // 寫法2：$row['sh']=($in_array($id,$_POST['sh']))?1:0; 
-  // (1)還是有問題，如果全部都不勾選 值沒有送出去$_POST['sh']會變成undefined
-  // (2)要加上isset()判斷，同上方寫法if($_POST['del']
+    // 步驟2：修改$row['sh'] 可多選  之前sh不是陣列sh[]
+    // 修改三元運算式
 
-  // 寫法3：先判斷 顯示是否存在 if如果有存在且有id才設為1 不存在則為0。如果全部都沒勾選 沒有資料送出->全部不存在->全部不顯示
+    // 寫法1：$row['sh']=($_POST['sh']==$id)?1:0;
+    // 要加上in_array() 確認迴圈['id'] 是否在$_POST['sh']陣列裡面 ~ 還要搭派isset()是否存在 一起使用
 
-  $row['sh']=(isset($_POST['sh']) && in_array($id,$_POST['sh']))?1:0; 
+    // 寫法2：$row['sh']=($in_array($id,$_POST['sh']))?1:0; 
+    // (1)還是有問題，如果全部都不勾選 值沒有送出去$_POST['sh']會變成undefined
+    // (2)要加上isset()判斷，同上方寫法if($_POST['del']
+
+    // 寫法3：先判斷 顯示是否存在 if如果有存在且有id才設為1 不存在則為0。如果全部都沒勾選 沒有資料送出->全部不存在->全部不顯示
+
+    $row['sh'] = (isset($_POST['sh']) && in_array($id, $_POST['sh'])) ? 1 : 0;
 
 
-  // 步驟6：如何知道新增或更新？因為有id
-  $Ad->save($row);
+    // 步驟6：如何知道新增或更新？因為有id
+    $Ad->save($row);
 
-  // 印出 存到資料表的$row
-  dd($row);
-
+    // 印出 存到資料表的$row
+    dd($row);
   }
-
 }
 
 // 步驟2：do=title 改成 do=ad
@@ -62,7 +59,7 @@ to("../backend.php?do=ad");
 ?>
 
 <script>
-/** 
+  /** 
 1. 出現意外狀況 程式寫完後，要先執行刪除 再點選單選框就正常
 剛寫完測試 點選單選框後，按下"修改確定"後，單選框顯示會消失不見
 (有可能 同時選到顯示跟刪除)
@@ -71,19 +68,8 @@ to("../backend.php?do=ad");
 實務上要多寫很多檢查程式 自動判斷資料有無如何處理 或定位有變更才處理
 規模不同 解決問題步驟有差異
 
-3. 程式優化：再思考哪些功能可以合併，參6/27筆記-第120行
+3. 思考程式優化：再思考哪些功能可以合併，參6/27筆記-第120行
 
-.\api\edit_ad.php
-.\api\edit_title.php
-
-.\api\insert_title.php
-.\api\insert_ad.php
-(1) 程式大多重複，只有少數不同的地方 可設參數
-(2) 最後api只需要五個檔案，即可解決所有後台需求
-(3) 功能變多10-20個時，逐個寫太花時間 也容易出錯 負擔加重
-(4) 擴充功能 要刻畫面 寫api、modal 有更快方式解決
-(5) 用判斷式if / switch case、迴圈 排除特例或做特別處理
-簡化重複多的程式碼
 
 */
 </script>
--- web01-1/api/insert_title.php
+++ web01-1/api/insert_title.php
@@ -17,22 +17,22 @@ echo "</pre>";
 // !empty() = 檢查是否「不是空的」 代表 上傳成功
 // 意思：如果用戶真的有選檔案上傳，就執行下面的程式
 if (!empty($_FILES['img']['tmp_name'])) {
-    
+
     // 步驟2 移動 / move_uploaded_file( ) 移動 上傳完成檔案 到 指定目錄下，並指定檔名
     // 兩個參數 ( $from , $to )
     // 把檔案從暫存區移到 ../images/ 資料夾
     // $_FILES['img']['name'] = 檔案的原始名稱 可能帶副檔名
     // 合併寫法不知是否可行 "../images/{$_FILES['img']['name']}"
-    move_uploaded_file($_FILES['img']['tmp_name'], "../images/".$_FILES['img']['name']);
-    
+    move_uploaded_file($_FILES['img']['tmp_name'], "../images/" . $_FILES['img']['name']);
+
     // 步驟3 記錄 / 記錄檔案名稱 $_POST 同$_FILES 都是 陣列變數
     // 把右方檔案名稱 存到 $_POST['img'] 變數中  之後要送到資料庫
     // $_POST['img'] 鍵值來自 <input type="file" name="img">
     // 一維陣列-儲存 表單欄位/鍵值  鍵值是 欄位名稱['img']['text']
-    
+
     // 補齊$_POST資料  對應資料表titles 欄位 ['img'] ['sh']
     // 郵差 接收表單資料 準備 投遞送到資料庫
-    $_POST['img']=$_FILES['img']['name'];
+    $_POST['img'] = $_FILES['img']['name'];
 
     // 所有上傳照片 預設不顯示 之後再手動調整
     // 這段可以移出判斷式
@@ -41,7 +41,7 @@ if (!empty($_FILES['img']['tmp_name'])) {
 
 // 步驟4 
 // 所有上傳照片 預設不顯示 之後再手動調整
-$_POST['sh']=0;
+$_POST['sh'] = 0;
 
 // 步驟5 DB::save()
 // 資料表目前是空的 需要先判斷資料表狀況 決定第一筆上傳資料開或關
@@ -53,3 +53,32 @@ $Title->save($_POST);
 // 步驟6 回到後台 ?do=title 注意?
 // 因為後台./madal/title.php發請求到api 需再將頁面導回後台
 to('../backend.php?do=title');
+
+?>
+
+<script>
+    /* 
+1. 出現意外狀況 程式寫完後，要先執行刪除 再點選單選框就正常
+剛寫完測試 點選單選框後，按下"修改確定"後，單選框顯示會消失不見
+(有可能 同時選到顯示跟刪除)
+
+2. 檢定與實務差異：檢定寫法是全部跑一次 全部處理
+實務上要多寫很多檢查程式 自動判斷資料有無如何處理 或定位有變更才處理
+規模不同 解決問題步驟有差異
+
+3. 程式優化：再思考哪些功能可以合併，參6/27筆記-第120行
+
+.\api\edit_ad.php
+.\api\edit_title.php
+
+.\api\insert_title.php
+.\api\insert_ad.php
+(1) 程式大多重複，只有少數不同的地方 可設參數
+(2) 最後api只需要五個檔案，即可解決所有後台需求
+(3) 功能變多10-20個時，逐個寫太花時間 也容易出錯 負擔加重
+(4) 擴充功能 要刻畫面 寫api、modal 有更快方式解決
+(5) 用判斷式if / switch case、迴圈 排除特例或做特別處理
+簡化重複多的程式碼
+
+*/
+</script>

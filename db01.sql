208314e5006f9f18cf4440e026ffffc9781fb17e
meganli <lovecezanne@gmail.com>
2025-09-05 16:30:08 +0800

    更新資料表

--- db01.sql
+++ db01.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- 主機： 127.0.0.1
--- 產生時間： 2025-09-02 01:00:34
+-- 產生時間： 2025-09-05 10:29:43
 -- 伺服器版本： 10.4.32-MariaDB
 -- PHP 版本： 8.2.12
 
@@ -41,7 +41,29 @@ INSERT INTO `ad` (`id`, `text`, `sh`) VALUES
 (1, '轉知臺北教育大學與臺灣師大合辦第11屆麋研齋全國硬筆書法比賽活動', 1),
 (2, '轉知:法務部辦理「第五屆法規知識王網路闖關競賽辦法', 0),
 (3, '轉知2012年全國青年水墨創作大賽活動', 0),
-(5, '轉知:教育是人類升沉的樞紐-2013教師生命成長營轉知:教育是人類升沉的樞紐-2013教師生命成長營', 1);
+(5, '轉知:教育是人類升沉的樞紐-2013教師生命成長營轉知:教育是人類升沉的樞紐-2013教師生命成長營', 1),
+(6, 'test', 1);
+
+-- --------------------------------------------------------
+
+--
+-- 資料表結構 `mvim`
+--
+
+CREATE TABLE `mvim` (
+  `id` int(10) UNSIGNED NOT NULL COMMENT '流水號',
+  `img` text DEFAULT NULL COMMENT '檔名/路徑',
+  `sh` int(1) UNSIGNED DEFAULT NULL COMMENT '顯示'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- 傾印資料表的資料 `mvim`
+--
+
+INSERT INTO `mvim` (`id`, `img`, `sh`) VALUES
+(1, '01C01.gif', 1),
+(2, '01C02.gif', 1),
+(3, '01C03.gif', 0);
 
 -- --------------------------------------------------------
 
@@ -63,7 +85,9 @@ CREATE TABLE `title` (
 INSERT INTO `title` (`id`, `text`, `img`, `sh`) VALUES
 (2, '222卓越科技大學校園資訊系統', '01B02.jpg', 0),
 (3, '333卓越科技大學校園資訊系統', '01B03.jpg', 0),
-(4, '卓越科技大學校園資訊系統', '01B04.jpg', 1);
+(4, '卓越科技大學校園資訊系統', '01B04.jpg', 1),
+(7, 'test', '01B01.jpg', 0),
+(8, 'test', '01B01.jpg', 0);
 
 --
 -- 已傾印資料表的索引
@@ -73,6 +97,12 @@ INSERT INTO `title` (`id`, `text`, `img`, `sh`) VALUES
 -- 資料表索引 `ad`
 --
 ALTER TABLE `ad`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- 資料表索引 `mvim`
+--
+ALTER TABLE `mvim`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -89,13 +119,19 @@ ALTER TABLE `title`
 -- 使用資料表自動遞增(AUTO_INCREMENT) `ad`
 --
 ALTER TABLE `ad`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT COMMENT '流水號', AUTO_INCREMENT=6;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT COMMENT '流水號', AUTO_INCREMENT=7;
+
+--
+-- 使用資料表自動遞增(AUTO_INCREMENT) `mvim`
+--
+ALTER TABLE `mvim`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT COMMENT '流水號', AUTO_INCREMENT=5;
 
 --
 -- 使用資料表自動遞增(AUTO_INCREMENT) `title`
 --
 ALTER TABLE `title`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT COMMENT '流水號', AUTO_INCREMENT=7;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT COMMENT '流水號', AUTO_INCREMENT=9;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;

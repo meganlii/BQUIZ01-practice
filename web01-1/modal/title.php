74cff0ea26d09de106b13d37040e460ff4987d2d
meganli <lovecezanne@gmail.com>
2025-09-05 11:05:59 +0800

    複習跨檔案路徑關係

--- web01-1/modal/title.php
+++ web01-1/modal/title.php
@@ -21,21 +21,24 @@
   </div>
 
   <div>
-    <input type="hidden" name="table" value="<?=$_GET['table'];?>">
+    <input type="hidden" name="table" value="<?= $_GET['table']; ?>">
     <input type="submit" value="新增">
     <input type="reset" value="重置">
   </div>
 </form>
 
 <script>
-  /**
+/**
 
 // 步驟2 
 1. 新增 <form> 參數  表單資料post送到api處理 action="./api/insert_title.php"
-2. 路徑不是../api  因為彈出視窗點開  網址在 backend.php 屬於./當前目錄 web01-1目錄
-3. 其他資料夾modal、backend檔案 送到api處理  要以backend.php角度來看跟api關係  屬於./當前目錄  web01-1目錄 
-api處理完了，資料夾api內的檔案跟backend.php關係 則是回上一層  用../  回到後台 
-4. "multipart/form-data" 多媒體格式/表單資料 配合type="file"才會用編碼方式傳送file 一般表單不用加
+2. 路徑關係
+(1) 不是../api  因為彈出視窗點開  網址在 backend.php 屬於./當前目錄 web01-1目錄
+其他資料夾modal、backend檔案 送到api處理  要以backend.php角度來看跟api關係  屬於./當前目錄  web01-1目錄 
+api處理完了
+(2) 資料夾api內檔案跟backend.php關係 則是回上一層  用../  回到後台
+
+3. "multipart/form-data" 多媒體格式/表單資料 配合type="file"才會用編碼方式傳送file 一般表單不用加
 
 // 步驟3
 1. 第08行  更改 action=./api/insert.php

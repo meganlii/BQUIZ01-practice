53393ea3d8a68f8061f7918131dd78da7ceceb8f
meganlii <lovecezanne@gmail.com>
2025-09-07 01:06:45 +0800

    6/27-7 建立完整需要用到的資料表

--- db01.sql
+++ db01.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- 主機： 127.0.0.1
--- 產生時間： 2025-09-05 10:29:43
+-- 產生時間： 2025-09-06 19:02:55
 -- 伺服器版本： 10.4.32-MariaDB
 -- PHP 版本： 8.2.12
 
@@ -29,7 +29,7 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `ad` (
   `id` int(10) UNSIGNED NOT NULL COMMENT '流水號',
-  `text` text DEFAULT NULL COMMENT '動態文字廣告',
+  `text` text DEFAULT NULL COMMENT '文字',
   `sh` int(1) UNSIGNED DEFAULT NULL COMMENT '顯示'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
@@ -43,6 +43,62 @@ INSERT INTO `ad` (`id`, `text`, `sh`) VALUES
 (3, '轉知2012年全國青年水墨創作大賽活動', 0),
 (5, '轉知:教育是人類升沉的樞紐-2013教師生命成長營轉知:教育是人類升沉的樞紐-2013教師生命成長營', 1),
 (6, 'test', 1);
+
+-- --------------------------------------------------------
+
+--
+-- 資料表結構 `admin`
+--
+
+CREATE TABLE `admin` (
+  `id` int(10) UNSIGNED NOT NULL COMMENT '流水號',
+  `acc` text DEFAULT NULL COMMENT '帳號',
+  `pw` text DEFAULT NULL COMMENT '密碼'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- 資料表結構 `bottom`
+--
+
+CREATE TABLE `bottom` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `bottom` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- 傾印資料表的資料 `bottom`
+--
+
+INSERT INTO `bottom` (`id`, `bottom`) VALUES
+(1, '卓越科技大學校園資訊系統');
+
+-- --------------------------------------------------------
+
+--
+-- 資料表結構 `image`
+--
+
+CREATE TABLE `image` (
+  `id` int(10) UNSIGNED NOT NULL COMMENT '流水號',
+  `img` text DEFAULT NULL COMMENT '檔名/路徑',
+  `sh` int(1) UNSIGNED DEFAULT NULL COMMENT '顯示'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- 資料表結構 `menu`
+--
+
+CREATE TABLE `menu` (
+  `id` int(10) UNSIGNED NOT NULL COMMENT '流水號',
+  `text` text DEFAULT NULL COMMENT '文字',
+  `href` text DEFAULT NULL COMMENT '檔名/路徑',
+  `main_id` int(10) UNSIGNED NOT NULL COMMENT '主選單id',
+  `sh` int(1) UNSIGNED DEFAULT NULL COMMENT '顯示'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- --------------------------------------------------------
 
@@ -61,9 +117,22 @@ CREATE TABLE `mvim` (
 --
 
 INSERT INTO `mvim` (`id`, `img`, `sh`) VALUES
-(1, '01C01.gif', 1),
+(1, '01C04.gif', 1),
 (2, '01C02.gif', 1),
-(3, '01C03.gif', 0);
+(3, '01C03.gif', 0),
+(5, '01C04.gif', 1);
+
+-- --------------------------------------------------------
+
+--
+-- 資料表結構 `news`
+--
+
+CREATE TABLE `news` (
+  `id` int(10) UNSIGNED NOT NULL COMMENT '流水號',
+  `text` text DEFAULT NULL COMMENT '文字',
+  `sh` int(1) UNSIGNED DEFAULT NULL COMMENT '顯示'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- --------------------------------------------------------
 
@@ -73,7 +142,7 @@ INSERT INTO `mvim` (`id`, `img`, `sh`) VALUES
 
 CREATE TABLE `title` (
   `id` int(10) UNSIGNED NOT NULL COMMENT '流水號',
-  `text` text DEFAULT NULL COMMENT '替代文字',
+  `text` text DEFAULT NULL COMMENT '文字',
   `img` text DEFAULT NULL COMMENT '檔名/路徑',
   `sh` int(1) UNSIGNED DEFAULT NULL COMMENT '顯示'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
@@ -83,11 +152,28 @@ CREATE TABLE `title` (
 --
 
 INSERT INTO `title` (`id`, `text`, `img`, `sh`) VALUES
-(2, '222卓越科技大學校園資訊系統', '01B02.jpg', 0),
+(2, '222卓越科技大學校園資訊系統', '01B01.jpg', 0),
 (3, '333卓越科技大學校園資訊系統', '01B03.jpg', 0),
 (4, '卓越科技大學校園資訊系統', '01B04.jpg', 1),
-(7, 'test', '01B01.jpg', 0),
-(8, 'test', '01B01.jpg', 0);
+(7, 'test', '01B02.jpg', 0);
+
+-- --------------------------------------------------------
+
+--
+-- 資料表結構 `total`
+--
+
+CREATE TABLE `total` (
+  `id` int(10) UNSIGNED NOT NULL COMMENT '流水號',
+  `total` int(10) UNSIGNED NOT NULL COMMENT '訪客數'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- 傾印資料表的資料 `total`
+--
+
+INSERT INTO `total` (`id`, `total`) VALUES
+(1, 200);
 
 --
 -- 已傾印資料表的索引
@@ -100,15 +186,51 @@ ALTER TABLE `ad`
   ADD PRIMARY KEY (`id`);
 
 --
+-- 資料表索引 `admin`
+--
+ALTER TABLE `admin`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- 資料表索引 `bottom`
+--
+ALTER TABLE `bottom`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- 資料表索引 `image`
+--
+ALTER TABLE `image`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- 資料表索引 `menu`
+--
+ALTER TABLE `menu`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- 資料表索引 `mvim`
 --
 ALTER TABLE `mvim`
   ADD PRIMARY KEY (`id`);
 
 --
+-- 資料表索引 `news`
+--
+ALTER TABLE `news`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- 資料表索引 `title`
 --
 ALTER TABLE `title`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- 資料表索引 `total`
+--
+ALTER TABLE `total`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -122,16 +244,52 @@ ALTER TABLE `ad`
   MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT COMMENT '流水號', AUTO_INCREMENT=7;
 
 --
+-- 使用資料表自動遞增(AUTO_INCREMENT) `admin`
+--
+ALTER TABLE `admin`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT COMMENT '流水號';
+
+--
+-- 使用資料表自動遞增(AUTO_INCREMENT) `bottom`
+--
+ALTER TABLE `bottom`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- 使用資料表自動遞增(AUTO_INCREMENT) `image`
+--
+ALTER TABLE `image`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT COMMENT '流水號';
+
+--
+-- 使用資料表自動遞增(AUTO_INCREMENT) `menu`
+--
+ALTER TABLE `menu`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT COMMENT '流水號';
+
+--
 -- 使用資料表自動遞增(AUTO_INCREMENT) `mvim`
 --
 ALTER TABLE `mvim`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT COMMENT '流水號', AUTO_INCREMENT=5;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT COMMENT '流水號', AUTO_INCREMENT=6;
+
+--
+-- 使用資料表自動遞增(AUTO_INCREMENT) `news`
+--
+ALTER TABLE `news`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT COMMENT '流水號';
 
 --
 -- 使用資料表自動遞增(AUTO_INCREMENT) `title`
 --
 ALTER TABLE `title`
   MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT COMMENT '流水號', AUTO_INCREMENT=9;
+
+--
+-- 使用資料表自動遞增(AUTO_INCREMENT) `total`
+--
+ALTER TABLE `total`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT COMMENT '流水號', AUTO_INCREMENT=2;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
--- web01-1/modal/update.php
+++ web01-1/modal/update.php
@@ -29,8 +29,8 @@ strreplace也可  但要換三個 有點囉嗦  沒有快很多
   // }
 
   // 步驟3：更簡化方式：改成陣列key=value形式處理 不用寫判斷條件式
-  // (1)宣告二維陣列 選單名稱+兩組k-v 寫好格式：[''=> []];
-  // (2)第二個陣列 有兩個內容 記得中間用逗號,隔開''=>'', 第二個陣列]，後方也有,
+  // (1)宣告二維陣列 選單名稱+兩組k-v 寫好格式  記得家分號；  [''=> [] ];
+  // (2)第二個陣列 有兩個內容 記得中間用逗號,隔開  ''=>'', 第二個陣列]，後方也有逗號,
   // (3)複製3組  調整對齊 寫入3組選單名稱
 
   $str = [
@@ -55,7 +55,6 @@ strreplace也可  但要換三個 有點囉嗦  沒有快很多
   // 應該用get打成POST
   // echo $str['tille']['header']
   // echo $str[$_POST['table']]['header']
-
   
 ?>
 
@@ -66,6 +65,7 @@ strreplace也可  但要換三個 有點囉嗦  沒有快很多
 </h3>
 
 <hr>
+
 
 <form action="./api/update.php" method="post" enctype="multipart/form-data">
   <div>
@@ -91,5 +91,14 @@ strreplace也可  但要換三個 有點囉嗦  沒有快很多
 1. 修改 .\backend\mvim.php   33行onclick路徑 改成 './modal/update.php
 2. 修改 .\backend\title.php  33行onclick路徑 改成 './modal/update.php
 3. 沒有改成$do參數  避免一行程式 重複帶一樣參數
+-->
+
+<!-- 步驟7
+1. 6/27-7 建立完整需要用到的資料表
+選單4-image  資料表-複製選單3-mvim
+選單7-news   資料表-複製選單2-ad
+選單8-admin  資料表-複製選單1-title
+選單9-menu   資料表-複製title 因為欄位最多
+選單5 跟 選單6  比較特別，獨立製表 手動放一筆資料進去
 -->
 
